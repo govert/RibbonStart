9470097247177d8e5140686ea2063ff6ddc8f00d
Govert van Drimmelen <govert@dnakode.com>
2024-03-22 20:19:29 +0200

    Add project files.

---
+++ MyFunctions.cs
@@ -0,0 +1,13 @@
+﻿using ExcelDna.Integration;
+
+namespace RibbonStart
+{
+    public static class MyFunctions
+    {
+        [ExcelFunction(Description = "My first .NET function")]
+        public static string SayHello(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
